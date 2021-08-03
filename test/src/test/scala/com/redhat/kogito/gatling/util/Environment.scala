e898afaca38716b8bcf23cf62020888cd0a20b3d
Daniele Martinoli <dmartino@redhat.com>
2021-08-03 10:41:17 +0200

    Set the OCP URL

--- test/src/test/scala/com/redhat/kogito/gatling/util/Environment.scala
+++ test/src/test/scala/com/redhat/kogito/gatling/util/Environment.scala
@@ -3,7 +3,8 @@
 package com.redhat.kogito.gatling.util
 
 object Environemnt {
-  val baseURL = scala.util.Properties.envOrElse("baseURL", "http://localhost:8080")
+  // val baseURL = scala.util.Properties.envOrElse("baseURL", "http://localhost:8080")
+  val baseURL = scala.util.Properties.envOrElse("baseURL", "http://process-quarkus-example-dmartino-kogito-benchmarking.apps.mw-ocp4.cloud.lab.eng.bos.redhat.com/")
 
   val numberOfUsers = scala.util.Properties.envOrElse("numberOfUsers", "5")
   val duration = scala.util.Properties.envOrElse("duration", "15")
