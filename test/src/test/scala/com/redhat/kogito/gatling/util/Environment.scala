236c2b07727ca47bb55e27a6381bb15b265734da
uteegozi <uegozi@redhat.com>
2021-08-17 12:54:16 +0300

    Update Environment.scala

--- test/src/test/scala/com/redhat/kogito/gatling/util/Environment.scala
+++ test/src/test/scala/com/redhat/kogito/gatling/util/Environment.scala
@@ -8,7 +8,7 @@ object Environemnt {
   val baseURL: String = Properties.envOrElse("baseURL", "http://process-quarkus-example-dmartino-kogito-benchmarking.apps.mw-ocp4.cloud.lab.eng.bos.redhat.com/")
 
   val numberOfUsers:Int = Properties.envOrElse("numberOfUsers", "5").toInt
-  val duration:Int = Properties.envOrElse("duration", "15").toInt
+  val duration:Int = Properties.envOrElse("duration", "5").toInt
   val maxDuration:Int = Properties.envOrElse("maxDuration", "5").toInt
 
 
