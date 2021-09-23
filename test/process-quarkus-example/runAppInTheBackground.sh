312ede421ccfa022aa408915977b1a1360001b3c
Lokesh Rangineni <lokeshforjava@gmail.com>
2021-09-23 12:54:16 -0400

    changed the default profile to mongo in runAppInTheBackground.sh file.

--- test/process-quarkus-example/runAppInTheBackground.sh
+++ test/process-quarkus-example/runAppInTheBackground.sh
@@ -1,3 +1,3 @@
-mvn -Dquarkus.profile=infinispan -P infinispan clean package
-nohup java -Dquarkus.profile=infinispan -jar target/quarkus-app/quarkus-run.jar > process-quarkus-example.log 2>&1 &
-echo $! > save_pid.txt:q
+mvn -Dquarkus.profile=mongo -Dquarkus.http.host=`hostname` -P mongo clean package
+nohup java -Dquarkus.profile=mongo -jar target/quarkus-app/quarkus-run.jar > process-quarkus-example.log 2>&1 &
+echo $! > save_pid.txt
