6802cd2dcd597c2b091028667e3db15d7915d572
ute-egozi <uegozi@redhat.com>
2021-10-14 09:00:08 +0300

    reminder of what to set if accessing application with localhost instead of hostname

--- test-apps/process-quarkus-example/runAppInTheBackground.sh
+++ test-apps/process-quarkus-example/runAppInTheBackground.sh
@@ -1,3 +1,4 @@
+echo 'to make application accessible as localhost, set -Dquarkus.http.host=0.0.0.0'
 mvn -Dquarkus.profile=mongo -Dquarkus.http.host=`hostname` -P mongo clean package
 nohup java -Dquarkus.profile=mongo -jar target/quarkus-app/quarkus-run.jar > process-quarkus-example.log 2>&1 &
 echo $! > save_pid.txt
