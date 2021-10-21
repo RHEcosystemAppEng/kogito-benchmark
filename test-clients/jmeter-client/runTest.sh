d2e34c515a77b96dca0e83cc22128b5287bb364c
ute-egozi <uegozi@redhat.com>
2021-10-21 09:49:56 +0300

    added warmup part and changed inout data to one file batch.json

--- test-clients/jmeter-client/runTest.sh
+++ test-clients/jmeter-client/runTest.sh
@@ -1,59 +1,81 @@
 #!/bin/sh
 
-TEST_DATA=$1
-TEST_DATA='{"replicas":1, "users":1, "requests":2}'
-TEST_NO=1
-#save as file since needed later for report
-echo $TEST_DATA >> test-run/test$TEST_NO.json
-
+TEST_IDX=$1
 TEST_FILE=test-run/batch.json
 JMETER_HOME=$( cat jmeterHome.txt )
 
-#echo $JMETER_HOME $TEST_DATA $TEST_FILE
+echo "***JMETER_HOME: "$JMETER_HOME "***TEST_FILE: "$TEST_FILE "***TEST_IDX: "$TEST_IDX
 
-EP_SCHEMA=$(jq '.Process .schema' $TEST_FILE)
-EP_URL=$(jq '.Process .url' $TEST_FILE)
+EP_SCHEMA=$(jq -r '.Process .schema' $TEST_FILE)
+EP_URL=$(jq -r '.Process .url' $TEST_FILE)
 EP_PORT=$(jq '.Process .port' $TEST_FILE)
 TEST_APP=$(jq -r '.Process .app' $TEST_FILE)
+WITH_WARMUP=$(jq -r '.Warmup .enabled' $TEST_FILE)
 
-#echo $EP_SCHEMA $EP_URL $EP_PORT $TEST_APP
+echo "***EP_SCHEMA: "$EP_SCHEMA "***EP_URL: "$EP_URL "***EP_PORT: "$EP_PORT "***TEST_APP: "$TEST_APP "***WITH_WARMUP: "$WITH_WARMUP
+
+REPLICAS=$(jq '.Tests['$TEST_IDX'] .replicas' $TEST_FILE)
+USERS=$(jq '.Tests['$TEST_IDX'] .users' $TEST_FILE)
+REQUESTS=$(jq '.Tests['$TEST_IDX'] .requests' $TEST_FILE)
+DURATION=$(jq '.Tests['$TEST_IDX'] .duration' $TEST_FILE)
+
+echo "***REPLICAS: "$REPLICAS "***USERS: "$USERS "***REQUESTS: "$REQUESTS "***DURATION: "$DURATION
 
 TEST_TYPE="NOT SET"
-REPLICAS=$(echo $TEST_DATA | jq '.replicas')
-USERS=$(echo $TEST_DATA | jq '.users')
-REQUESTS=0
-DURATION=0
-hasRequests=$(echo $TEST_DATA | jq 'has("requests")')
-hasDuration=$(echo $TEST_DATA | jq 'has("duration")')
-if [ $hasRequests = "true" ]
+if [ $REQUESTS != null ]
 then
   TEST_TYPE="requests"
-  REQUESTS=$(echo $TEST_DATA | jq '.requests')
-elif [ $hasDuration = "true" ]
+elif [ $DURATION != null ]
 then
   TEST_TYPE="duration"
-  DURATION=$(echo $TEST_DATA | jq '.duration')
 else
   echo "not a valid test_type found - expected requests or duration data"
   exit
 fi
-#echo $TEST_TYPE
+echo "***TEST_TYPE: "$TEST_TYPE
 
 if [ $TEST_APP != "order" ] && [ $TEST_APP != "simpleHT" ]
 then
   echo "not a valid TEST_APP found - expected order, simpleHT"
   exit
 fi
-#echo $TEST_APP
+echo "***TEST_APP: "$TEST_APP
 
-#all test files need to be named according to the following schema: hardcoded "Users"
-# + the test type which is either Duration or Requests
-# and then the test application which is order or simple
-TEST_CASE="Users"${TEST_TYPE^}${TEST_APP^}".jmx"
-#echo $TEST_CASE
+if [ $WITH_WARMUP = "yes" ]
+then
+	echo "**********************************************"
+	echo "************* starting warmup run *******************"
+
+	#warmup tests are always run on the same application like the real test, but different data
+	TEST_CASE_WARMUP="UsersDuration"${TEST_APP^}".jmx"
+	WARMUP_USERS=$(jq '.Warmup .users' $TEST_FILE)
+	WARMUP_DURATION=$(jq '.Warmup .duration' $TEST_FILE)
+	echo "***TEST_CASE_WARMUP: "$TEST_CASE_WARMUP "***WARMUP_USERS: "$WARMUP_USERS "***WARMUP_DURATION: "$WARMUP_DURATION
+
+	TEST_RUN="$JMETER_HOME/bin/jmeter -n -t $TEST_CASE_WARMUP \
+	-Jschema=$EP_SCHEMA \
+	-Jurl=$EP_URL \
+	-Jport=$EP_PORT \
+	-Jusers=$WARMUP_USERS \
+	-Jduration=$WARMUP_DURATION \
+	-Jrequests=0 \
+	-l test-run/results/res"$TEST_IDX"warmup.jtl"
+	echo "***TEST_RUN: "$TEST_RUN
+	$TEST_RUN
+
+	echo "**********************************************"
+	echo "************* finished warmup run *******************"
+fi
 
 echo "**********************************************"
 echo "************* starting run *******************"
+
+#all test files need to be named according to the following naming convention:
+# they start with hardcoded "Users"
+# + the test type which is either "duration" or "requests", first capital letter in file name
+# + the test application which is "order" or "simpleHT", first letter capital in file name
+TEST_CASE="Users"${TEST_TYPE^}${TEST_APP^}".jmx"
+echo "***TEST_CASE: "$TEST_CASE
 
 TEST_RUN="$JMETER_HOME/bin/jmeter -n -t $TEST_CASE \
 -Jschema=$EP_SCHEMA \
@@ -62,19 +84,18 @@ TEST_RUN="$JMETER_HOME/bin/jmeter -n -t $TEST_CASE \
 -Jusers=$USERS \
 -Jduration=$DURATION \
 -Jrequests=$REQUESTS \
--l test-run/results/res$TEST_NO.jtl"
-echo $TEST_RUN
+-l test-run/results/res$TEST_IDX.jtl"
+echo "***TEST_RUN: "$TEST_RUN
 $TEST_RUN
 
 echo "**********************************************"
 echo "************* finished run *******************"
 
 TEST_AGG="$JMETER_HOME/bin/JMeterPluginsCMD.sh \
---generate-csv test-run/results/res$TEST_NO.csv \
---input-jtl test-run/results/res$TEST_NO.jtl \
+--generate-csv test-run/results/res$TEST_IDX.csv \
+--input-jtl test-run/results/res$TEST_IDX.jtl \
 --plugin-type AggregateReport"
-echo $TEST_AGG
-
+echo "***TEST_AGG: "$TEST_AGG
 $TEST_AGG
 
 echo "**********************************************"
