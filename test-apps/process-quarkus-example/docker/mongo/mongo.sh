5a53f3a68159765381c8262c6be5dfd9ad4a03d3
ute-egozi <uegozi@redhat.com>
2021-11-01 15:51:31 +0200

    mongo db checks

---
+++ test-apps/process-quarkus-example/docker/mongo/mongo.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+WHEN=$1   # before or after
+CONTAINER_ENGINE=$2   # docker or podman
+if [ "$WHEN" = 'before' ]
+then
+  $CONTAINER_ENGINE cp mongoBefore.js kogito-mongodb:/tmp
+#  podman exec -it kogito-mongodb "mongo -u developer -p mypass < /tmp/mongoBefore.js"
+  $CONTAINER_ENGINE exec -it kogito-mongodb /bin/bash -c "mongo < /tmp/mongoBefore.js"
+  exit
+fi
+
+if [ "$WHEN" = 'after' ]
+then
+  $CONTAINER_ENGINE mongoAfter.js kogito-mongodb:/tmp
+#  podman exec -it kogito-mongodb "mongo -u developer -p mypass < /tmp/mongoBefore.js"
+  $CONTAINER_ENGINE exec -it kogito-mongodb /bin/bash -c "mongo < /tmp/mongoAfter.js"
+  exit
+fi
