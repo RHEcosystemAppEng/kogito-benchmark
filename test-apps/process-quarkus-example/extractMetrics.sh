86bcdb200f186f510376f9f7545a000e81d47654
ute-egozi <uegozi@redhat.com>
2021-11-04 18:22:31 +0200

    updated to some snapshot branch changes

--- test-apps/process-quarkus-example/extractMetrics.sh
+++ test-apps/process-quarkus-example/extractMetrics.sh
@@ -1,8 +1,14 @@
+TEST_IDX=$1
+SYSTEM_DATA=system-data.csv
+USAGE_DATA=usage$TEST_IDX.csv
+echo $USAGE_DATA $SYSTEM_DATA
+
 if [[ -f save_pid.txt ]]
 then
   PID=$(cat save_pid.txt)
   echo "Extracting system metrics for process(id) : "$PID
-  touch metrics.txt
+  rm -r metrics
+  mkdir metrics
 
   JAVA_VERSION=$(java -version 2>&1 | head -1 )
 
@@ -11,8 +17,10 @@ then
   TOTAL_MEMORY=$(grep MemTotal /proc/meminfo | awk '{print $2}' | xargs -I {} echo "scale=2; {}/1024^2" | bc)
 
   APP_CPU_USAGE=$(top -b -p $PID -n 1 | tail -n 1 | awk '{print $9}')
+  APP_CPU_USAGE=${APP_CPU_USAGE%.*}
+  APP_CPU_USAGE=$((APP_CPU_USAGE/4))
 
-  SYSTEM_CPU_USAGE_PERCENTAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.2f%%\n", prefix, 100 - v }')
+  SYSTEM_CPU_USAGE_PERCENTAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.2f\n", prefix, 100 - v }')
 
   APP_MEMORY_USAGE=$(top -b -p $PID -n 1 | tail -n 1 | awk '{print $6}' | xargs -I {} echo "scale=2; {}/1024" | bc)
 
@@ -24,7 +32,12 @@ then
 
   SYSTEM_MEMORY_USAGE_PERCENTAGE=$(echo "$SYSTEM_MEMORY_USAGE * 100" | xargs -I {} echo "scale=2; {}/$TOTAL_MEMORY_IN_MB" | bc)
 
-  echo "$JAVA_VERSION|$TOTAL_CPU CPU core|$TOTAL_MEMORY gb|$APP_CPU_USAGE|$SYSTEM_CPU_USAGE_PERCENTAGE|$APP_MEMORY_USAGE mb|$APP_MEMORY_USAGE_PERCENTAGE%|$SYSTEM_MEMORY_USAGE mb|$SYSTEM_MEMORY_USAGE_PERCENTAGE%" >> metrics.txt
+  echo "java-version, total-CPUs, total-Memory-GB" > metrics/$SYSTEM_DATA
+  echo "$JAVA_VERSION, $TOTAL_CPU, ${TOTAL_MEMORY}" >> metrics/$SYSTEM_DATA
+
+  echo "app-CPU-usage, system-CPU-usage, app-Memory-usage, app-Memory-usage-percentage, system-Memory-usage, system-Memory-usage-percentage" > metrics/$USAGE_DATA
+  echo "$APP_CPU_USAGE%, $SYSTEM_CPU_USAGE_PERCENTAGE%, ${APP_MEMORY_USAGE}MB, $APP_MEMORY_USAGE_PERCENTAGE%, ${SYSTEM_MEMORY_USAGE}MB, $SYSTEM_MEMORY_USAGE_PERCENTAGE%" >> metrics/$USAGE_DATA
+
 else
   echo "save_pid.txt file not found. Application has not properly started. Please run the application to collect metrics..."
 fi
--- test/runTests.sh
+++ test/runTests.sh
@@ -4,51 +4,37 @@ TEST_CLIENT=$1  # JMETER or QUARKUS
 TEST_IDX=$2  # either number or ALL
 BATCH_FILE=test-run/batch.json
 
-echo "test client "$TEST_CLIENT
-echo "test index "$TEST_IDX
+echo "test client $TEST_CLIENT"
+echo "test index $TEST_IDX"
 
 WITH_WARMUP=$(jq -r '.Warmup .enabled' $BATCH_FILE)
-NO_OF_TESTS=$(jq '.Tests | length' $BATCH_FILE)
+NO_OF_TESTS=$(jq '.Tests.runs | length' $BATCH_FILE)
 
-if [[ $TEST_CLIENT = "JMETER" ]]
+if [ "$TEST_CLIENT" = "JMETER" ]
 then
   cd ../test-clients/jmeter-client
-elif [[ $TEST_CLIENT = "QUARKUS" ]]
+elif [ "$TEST_CLIENT" = "QUARKUS" ]
 then
   cd ../test-clients/quarkus-client
 else
-  echo "invalid test client "$TEST_CLIENT
+  echo "invalid test client $TEST_CLIENT"
   exit
 fi
 
-if [[ $TEST_IDX = "ALL" ]]
+#run warmup
+if [ "$WITH_WARMUP" = "yes" ]
+then
+  ./runTest.sh "$TEST_IDX" "$WITH_WARMUP"
+fi
+
+if [ "$TEST_IDX" = "ALL" ]
 then
   TEST_COUNTER=0
-  while [ $TEST_COUNTER -lt $NO_OF_TESTS ]
+  while [ $TEST_COUNTER -lt "$NO_OF_TESTS" ]
   do
-    #run warmup
-    if [ $WITH_WARMUP = "yes" ]
-    then
-      ./runTestWarmup.sh $TEST_COUNTER
-    fi
-    #kickoff metrics collection - call Lokeshs REST API here - send interval for polling metrics on application, env (Vm or OCP) to use
-    #TODO
-    #run test
-    ./runTest.sh $TEST_COUNTER
-    #request accumulated metrics - call Lokeshs REST API here
-    #TODO
+    ./runTest.sh $TEST_COUNTER "no"
     TEST_COUNTER=$((TEST_COUNTER+1))
   done
 else
-    #run warmup
-    if [ $WITH_WARMUP = "yes" ]
-    then
-      ./runTestWarmup.sh $TEST_IDX
-    fi
-    #kickoff metrics collection - call Lokeshs REST API here - send interval for polling metrics on application, env (Vm or OCP) to use
-    #TODO
-    #run test
-    ./runTest.sh $TEST_IDX
-    #request accumulated metrics - call Lokeshs REST API here
-    #TODO
+    ./runTest.sh "$TEST_IDX" "no"
 fi
