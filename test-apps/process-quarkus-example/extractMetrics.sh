807dc347a7b29d847b740a112e4ff8181c20fa70
ute-egozi <uegozi@redhat.com>
2021-11-29 13:42:37 +0200

    fixed conversion error

--- test-apps/process-quarkus-example/extractMetrics.sh
+++ test-apps/process-quarkus-example/extractMetrics.sh
@@ -1,8 +1,40 @@
+#!/bin/bash
+
+TEST_IDX=$1
+SYSTEM_DATA=system-data.csv
+USAGE_DATA=usage$TEST_IDX.csv
+echo $USAGE_DATA $SYSTEM_DATA
+
+function getValueInMegaBytes {
+  input=$1
+  GB="g"
+  MB="mb"
+  KiB="kb"
+  if [[ "$input" == *$GB ]]; then
+    res=$input
+    res=${res/$GB/}
+    res=$(echo "scale=2; $res * 1024 * 1.00" | bc)
+    echo $res;
+  elif [[ "$input" == *$MB ]]; then
+    res=$input
+    res=${res/$MB/}
+    echo $res;
+  elif [[ "$input" == *$KiB ]]; then
+    res=$input
+    res=${res/$KiB/}
+    res=$(echo "scale=2; $res / 1024 * 1.00" | bc)
+    echo $res;
+  else
+    echo $input
+  fi
+}
+
 if [[ -f save_pid.txt ]]
 then
   PID=$(cat save_pid.txt)
   echo "Extracting system metrics for process(id) : "$PID
-  touch metrics.txt
+  rm -r metrics
+  mkdir metrics
 
   JAVA_VERSION=$(java -version 2>&1 | head -1 )
 
@@ -10,13 +42,15 @@ then
 
   TOTAL_MEMORY=$(grep MemTotal /proc/meminfo | awk '{print $2}' | xargs -I {} echo "scale=2; {}/1024^2" | bc)
 
-  APP_CPU_USAGE=$(top -b -p $PID -n 1 | tail -n 1 | awk '{print $9}')
+  APP_CPU_USAGE=$(top -b -p $PID -n 1 | tail -n 1 | awk '{print $9}' | xargs -I {} echo "{}/4" | bc)
 
-  SYSTEM_CPU_USAGE_PERCENTAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.2f%%\n", prefix, 100 - v }')
+  SYSTEM_CPU_USAGE_PERCENTAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.2f\n", prefix, 100 - v }')
 
-  APP_MEMORY_USAGE=$(top -b -p $PID -n 1 | tail -n 1 | awk '{print $6}' | xargs -I {} echo "scale=2; {}/1024" | bc)
+  APP_MEMORY_USAGE=$(top -b -p $PID -n 1 | tail -n 1 | awk '{print $6}')
+  APP_MEMORY_USAGE="$(getValueInMegaBytes $APP_MEMORY_USAGE)"
 
-  TOTAL_MEMORY_IN_MB=$(grep MemTotal /proc/meminfo | awk '{print $2}' | xargs -I {} echo "scale=2; {}/1024" | bc)
+  TOTAL_MEMORY_IN_MB=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+  TOTAL_MEMORY_IN_MB="$(getValueInMegaBytes $TOTAL_MEMORY_IN_MB)"
 
   APP_MEMORY_USAGE_PERCENTAGE=$(echo "$APP_MEMORY_USAGE * 100" | xargs -I {} echo "scale=2; {}/$TOTAL_MEMORY_IN_MB" | bc)
 
@@ -24,7 +58,12 @@ then
 
   SYSTEM_MEMORY_USAGE_PERCENTAGE=$(echo "$SYSTEM_MEMORY_USAGE * 100" | xargs -I {} echo "scale=2; {}/$TOTAL_MEMORY_IN_MB" | bc)
 
-  echo "$JAVA_VERSION|$TOTAL_CPU CPU core|$TOTAL_MEMORY gb|$APP_CPU_USAGE|$SYSTEM_CPU_USAGE_PERCENTAGE|$APP_MEMORY_USAGE mb|$APP_MEMORY_USAGE_PERCENTAGE%|$SYSTEM_MEMORY_USAGE mb|$SYSTEM_MEMORY_USAGE_PERCENTAGE%" >> metrics.txt
+  echo "java-version, total-CPUs, total-Memory-GB" > metrics/$SYSTEM_DATA
+  echo "$JAVA_VERSION, $TOTAL_CPU, ${TOTAL_MEMORY}" >> metrics/$SYSTEM_DATA
+
+  echo "app-CPU-usage, system-CPU-usage, app-Memory-usage, app-Memory-usage-percentage, system-Memory-usage, system-Memory-usage-percentage" > metrics/$USAGE_DATA
+  echo "$APP_CPU_USAGE%, $SYSTEM_CPU_USAGE_PERCENTAGE%, ${APP_MEMORY_USAGE}MB, $APP_MEMORY_USAGE_PERCENTAGE%, ${SYSTEM_MEMORY_USAGE}MB, $SYSTEM_MEMORY_USAGE_PERCENTAGE%" >> metrics/$USAGE_DATA
+
 else
   echo "save_pid.txt file not found. Application has not properly started. Please run the application to collect metrics..."
 fi
